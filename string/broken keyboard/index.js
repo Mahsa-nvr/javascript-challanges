f63d078a1e30b2a86777c5254d13e7539ac3cd41
mahsa <mahsaanvari2@gmail.com>
2023-05-22 14:49:31 +0430

    add question to broken keys

--- string/broken keyboard/index.js
+++ string/broken keyboard/index.js
@@ -1,3 +1,12 @@
+// Given what is supposed to be typed and what is actually typed, write a function that returns the broken key(s). The function looks like:
+
+// findBrokenKeys(correct phrase, what you actually typed)
+// findBrokenKeys("happy birthday", "hawwy birthday") ➞ ["p"]
+// Notes
+// Broken keys should be ordered by when they first appear in the sentence.
+// Only one broken key per letter should be listed.
+// Letters will all be in lower case.
+
 function findBrokenKeys(correct, typed) {
     let brokenKeys = new Set();
     for (let i=0; i<correct.length; i++) {
