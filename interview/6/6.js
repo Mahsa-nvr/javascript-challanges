5e312622ec4e205519495f25c94897dd542f1cf7
mahsa <mahsaanvari2@gmail.com>
2023-07-04 11:43:07 +0430

    interview 6

--- interview/6/6.js
+++ interview/6/6.js
@@ -9,3 +9,15 @@
 // Notes
 // Should be case insensitive.
 // The two given strings can be of different lengths.
+
+function isAnagram(str1, str2) {
+str1 = str1.toLowerCase();
+str2 = str2.toLowerCase();
+str1 = str1.replace(/[^a-z]/g, "");
+str2 = str2.replace(/[^a-z]/g, "");
+// Sort the characters of the strings alphabetically
+str1 = str1.split("").sort().join("");
+str2 = str2.split("").sort().join("");
+// Compare the sorted strings and return true or false
+return str1 === str2;
+}
