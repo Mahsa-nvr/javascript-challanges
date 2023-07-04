2dd096d97e82a41746cf86b8c45842390f9e5687
mahsa <mahsaanvari2@gmail.com>
2023-07-04 11:47:34 +0430

    add bigO explanaton to 6

--- interview/6/6.js
+++ interview/6/6.js
@@ -21,3 +21,7 @@ str2 = str2.split("").sort().join("");
 // Compare the sorted strings and return true or false
 return str1 === str2;
 }
+
+
+
+//The bigO of this code is O(n log n), where n is the length of the longer string. This is because the code performs a replace operation, which is O(n), and a sort operation, which is O(n log n), for each string. The replace operation is O(n) because it loops through all the characters of the string and replaces any non-alphabetic characters with an empty string. The sort operation is O(n log n) because it uses a comparison-based sorting algorithm, such as merge sort or quick sort, to sort the characters of the string alphabetically. The comparison of the sorted strings is O(1) because it is a simple equality check. The overall time complexity of the code is dominated by the sort operation, which is O(n log n).
